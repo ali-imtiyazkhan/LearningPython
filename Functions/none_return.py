58440b02f0db2df32d35dac0504449c68a1971ac
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-04-07 18:32:43 +0530

    none function logic added

---
+++ Functions/none_return.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("This function does not return any value.")
+
+# Calling the function and storing its result
+result = my_function()
+
+# Printing the result, which should be None
+print(f"The result of calling the function is: {result}")
